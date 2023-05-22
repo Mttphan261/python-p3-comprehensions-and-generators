a95ab522b49422f4f17cec717d31e34f153cd1cb
Matthew Phan <mttphan261@gmail.com>
2023-05-22 15:37:28 -0500

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_list = [int for int in num_list if int % 2 == 0]
+    return even_list
     pass
 
 def make_exclamation(sentence_list):
+    exclamation_list = [f'{n}!' for n in sentence_list ]
+    return exclamation_list
     pass
